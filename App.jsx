f05f5eb266e7f45c8505fc482db26ed56d754b30
hung <hungtang@kms-technology.com>
2017-12-27 17:27:12 +0700

    ok

--- App.jsx
+++ App.jsx
@@ -4,7 +4,7 @@ class App extends React.Component {
    render() {
       return (
          <div>
-            Hello world! hihi
+            Hello world! hihi what the hell?
          </div>
       );
    }
